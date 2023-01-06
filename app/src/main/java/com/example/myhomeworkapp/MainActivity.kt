a40a7ce1ba74c24d3d45a89b236b17c9e7abea98
yanabirukova <y.birukova@gmail.com>
2023-01-07 01:00:01 +0300

    homework17 is done

--- app/src/main/java/com/example/myhomeworkapp/MainActivity.kt
+++ app/src/main/java/com/example/myhomeworkapp/MainActivity.kt
@@ -3,30 +3,41 @@ package com.example.myhomeworkapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var textCounter: TextView
+    private lateinit var buttonCounter: Button
+    private lateinit var buttonOpenSecondActivity: Button
+    private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textCounter = findViewById(R.id.textView)
 
-        val buttonCounter = findViewById<TextView>(R.id.button_counter)
-        var count = 0
+        count = savedInstanceState?.getInt("COUNT") ?: 0
+
+        textCounter = findViewById(R.id.text_result)
+        buttonCounter = findViewById(R.id.button_counter)
+        buttonOpenSecondActivity = findViewById(R.id.button_open_second_activity)
+
         textCounter.text = count.toString()
 
         buttonCounter.setOnClickListener {
-            count++
-            textCounter.text = count.toString()
+            textCounter.text = (++count).toString()
         }
 
-        val buttonOpenSecondActivity = findViewById<TextView>(R.id.button_open_second_activity)
         buttonOpenSecondActivity.setOnClickListener {
             val someIntent = Intent(this, SecondActivity::class.java)
-            someIntent.putExtra("RESULT", textCounter.text)
+            someIntent.putExtra("RESULT", count)
             startActivity(someIntent)
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt("COUNT", count)
     }
 }
--- app/src/main/java/com/example/myhomeworkapp/SecondActivity.kt
+++ app/src/main/java/com/example/myhomeworkapp/SecondActivity.kt
@@ -5,15 +5,15 @@ import android.os.Bundle
 import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        val count = intent.getStringExtra("RESULT")
+        val count = intent.getIntExtra("RESULT", 0)
 
-        val textView = findViewById<TextView>(R.id.textView)
-        val defaultText = getString(R.string.results)
-        textView.text = "$defaultText $count"
+        textView = findViewById(R.id.textView_second_activity)
+        textView.text = "${getString(R.string.results)} $count"
     }
 }
